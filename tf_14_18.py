d805b2f9c929165ce4d716c710d58cfd556bf4bd
dasvootz <ericvoots@gmail.com>
2018-03-04 22:54:51 -0500

    just need to merge with conference ranks

--- tf_14_18.py
+++ tf_14_18.py
@@ -29,10 +29,34 @@ details2_df = details2_df.drop('TeamID', axis=1)
 details2_df = details2_df.rename(columns={'Seed': 'LSeed'})
 details2_df['LSeed'] = details2_df['LSeed'].str.lstrip('YXWZba').str.rstrip('YXWbZba')
 
+details2_df[['WSeed', 'LSeed']] = details2_df[['WSeed', 'LSeed']].astype(float)
+
 del t_seeds_df, details_df
 gc.collect()
 
-print(details2_df.head(25))
+#get conference of the team
+
+#winning teams conference
+details3_df = pd.merge(details2_df, teams_conf_df, how='left', left_on=['WTeamID', 'Season'], right_on=['TeamID', 'Season'])
+details3_df = details3_df.drop('TeamID', axis=1)
+details3_df = details3_df.rename(columns={'ConfAbbrev': 'WConf'})
+
+#losing team conference
+details3_df = pd.merge(details3_df, teams_conf_df, how='left', left_on=['LTeamID', 'Season'], right_on=['TeamID', 'Season'])
+details3_df = details3_df.drop('TeamID', axis=1)
+details3_df = details3_df.rename(columns={'ConfAbbrev': 'LConf'})
+
+del teams_conf_df, details2_df
+gc.collect()
+
+#get seed matching up information - play in games with two 16s will be a 0% due to data but its 16 seed so...
+details4_df = pd.merge(details3_df, seed_hist_df, how='left', left_on=['WSeed', 'LSeed'], right_on=['Seed1', 'Seed2'])
+details4_df = details4_df.drop(['Wins', 'Losses', 'Seed1', 'Seed2'], axis=1)
+
+del details3_df, seed_hist_df
+gc.collect()
+
+print(details4_df.head(25))
 
 
 
