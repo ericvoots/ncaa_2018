e3c97a4fa1aed97eca18636d9182661f8bdbea9a
dasvootz <ericvoots@gmail.com>
2018-03-04 22:31:56 -0500

    merged details and seeds

--- tf_14_18.py
+++ tf_14_18.py
@@ -3,6 +3,7 @@
 #import tensorflow
 import pandas as pd
 import numpy as np
+import gc
 
 t_seeds_df = pd.read_csv('input\\datafiles\\NCAATourneySeeds.csv')
 
@@ -10,4 +11,28 @@ details_df = pd.read_csv('input\\datafiles\\NCAATourneyDetailedResults.csv')
 
 teams_conf_df = pd.read_csv('input\\datafiles\\TeamConferences.csv')
 
-print(details_df.head(5))
+seed_hist_df = pd.read_csv('input\\seed_historical.csv')
+
+conf_rank_df = pd.read_csv('input\\conference_rank.csv', delimiter=';')
+
+#need to join all data to details
+
+#get winning team seed information
+details2_df = pd.merge(details_df, t_seeds_df, how='left', left_on=['WTeamID', 'Season'], right_on=['TeamID', 'Season'])
+details2_df = details2_df.drop('TeamID', axis=1)
+details2_df = details2_df.rename(columns={'Seed': 'WSeed'})
+details2_df['WSeed'] = details2_df['WSeed'].str.lstrip('YXWZba').str.rstrip('YXWbZba')
+
+#lossing team seed information
+details2_df = pd.merge(details2_df, t_seeds_df, how='left', left_on=['LTeamID', 'Season'], right_on=['TeamID', 'Season'])
+details2_df = details2_df.drop('TeamID', axis=1)
+details2_df = details2_df.rename(columns={'Seed': 'LSeed'})
+details2_df['LSeed'] = details2_df['LSeed'].str.lstrip('YXWZba').str.rstrip('YXWbZba')
+
+del t_seeds_df, details_df
+gc.collect()
+
+print(details2_df.head(25))
+
+
+
